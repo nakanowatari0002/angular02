e35e8026ef57f2a5c9f503076b5c8e75e4d739e8
nakanowatari0002 <nakanowatari0002@gmail.com>
2020-06-13 14:46:58 +0900

    ヘッダーコンポーネント追加

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,21 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TestComponent } from './testtag/test.component';
+import { FormsModule } from '@angular/forms';
+import { NavbarComponent } from './common/navbar/navbar.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TestComponent,
+    NavbarComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/testtag/test.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'test-app',
+    templateUrl: 'test.component.html'
+  })
+export class TestComponent {
+  show=false;
+}
